6701fece576165e5882c8788020a1dc5c12ab471
Nirmal Kumar <nirmal.kumar@comprotechnologies.com>
2019-09-16 19:48:43 +0530

    prep for heroku

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const morgan = require('morgan');
 
 const config = require('./config');
 const routes = require('./routes');
@@ -12,6 +13,7 @@ const port = process.env.PORT || config.port;
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs');
 
+app.use(morgan('dev'));
 app.use(bodyParser.json({ limit: '1mb', type: '*/*' }));
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
